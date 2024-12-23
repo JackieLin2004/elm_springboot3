e7e220c5cb74b8073d8ce1a8aacb0410b7dcf705
JackieLin2004 <linxy20040304@163.com>
2024-12-24 02:11:14 +0800

    Configuration of docker and basic environment

---
+++ src/main/java/ynu/jackielin/elm/ElmSpringboot3Application.java
@@ -0,0 +1,13 @@
+package ynu.jackielin.elm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ynu/jackielin/elm/ElmSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.jackielin.elm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
