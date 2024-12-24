b808b5fd1a84e7ffae038a29be377a732eefbe29
JackieLin2004 <linxy20040304@163.com>
2024-12-24 17:22:00 +0800

    Some necessary filters

--- src/test/java/ynu/jackielin/elm/ElmSpringboot3ApplicationTests.java
+++ src/test/java/ynu/jackielin/elm/ElmSpringboot3ApplicationTests.java
@@ -2,12 +2,14 @@ package ynu.jackielin.elm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class ElmSpringboot3ApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
     }
 
 }
