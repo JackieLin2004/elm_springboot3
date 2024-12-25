85319d9da9898e064afded66141e4f5178d3e051
JackieLin2004 <linxy20040304@163.com>
2024-12-25 21:46:31 +0800

    Design orders table

---
+++ src/main/java/ynu/jackielin/elm/entity/po/Orders.java
@@ -0,0 +1,28 @@
+package ynu.jackielin.elm.entity.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@TableName("orders")
+@AllArgsConstructor
+public class Orders {
+    @TableId(type = IdType.AUTO)
+    Long orderId;
+    @TableField("userId")
+    Long userId;
+    @TableField("businessId")
+    Long businessId;
+    @TableField("orderDate")
+    String orderDate;
+    @TableField("orderTotal")
+    Double orderTotal;
+    @TableField("daId")
+    Long daId;
+    @TableField("orderState")
+    Integer orderState;
+}
